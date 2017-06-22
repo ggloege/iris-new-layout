c1c95835028dbe30acdcf76d060360daa59412e2
Gabriel Gloege <gabe@Gabes-MacBook-Air.local>
2017-06-22 13:33:51 -0400

    Add toggle functions
    Make search bar hideable. Fire an alert when pressing the delete button
    on a card.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
 function toggleWindow() {
   let windowDisplay = document.getElementById('window');
-  console.log("The current value of windowDisplay is " + windowDisplay.style.display);
-  if (windowDisplay.style.display === 'none') {
-    windowDisplay.style.display = 'inline';
-  } else {
-    windowDisplay.style.display = 'none';
-  }
-  console.log("Just tried to toggle the window.")
+  console.log(windowDisplay.style.display);
+  windowDisplay.style.display === 'inline' ? windowDisplay.style.display = 'none' : windowDisplay.style.display = 'inline';
+  console.log("Just tried to toggle the window.");
 };
 
 function alertCheck() {
-  alert("This is working!");
+  alert("This button is firing a function!");
+};
+
+function toggleSearch() {
+  let searchBar = document.getElementById('object-search');
+  searchBar.style.display === 'block' ? searchBar.style.display = 'none' : searchBar.style.display = 'block';
+  console.log("Just tried to toggle the search bar.");
+};
+
+function dangerAlert() {
+  alert("This will destroy this learning object forever. You sure you want to do that?");
+};
+
+function doubleAlert() {
+  alertCheck();
+  dangerAlert();
 }

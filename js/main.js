30aee8e775cd40020664afbc487e6a9809586f4c
Gabriel Gloege <gabe@Gabes-MacBook-Air.local>
2017-06-23 16:04:21 -0400

    Playing with o'clock functions

--- js/main.js
+++ js/main.js
@@ -23,3 +23,31 @@ function doubleAlert() {
   alertCheck();
   dangerAlert();
 }
+
+function hideFeed() {
+  let feed = document.getElementsByClassName('learning-object');
+  console.log(feed);
+  for (var i = 0; i < feed.length; i++) {
+    feed[i].style.display = 'none';
+  };
+}
+
+function showFeed() {
+  let feed = document.getElementsByClassName('learning-object');
+  console.log(feed);
+  for (var i = 0; i < feed.length; i++) {
+    feed[i].style.display = 'block';
+  };
+}
+
+
+function showFilteredFeed(objectType) {
+  let feed = document.getElementsByClassName('learning-object');
+  console.log(feed);
+  for (var i = 0; i < feed.length; i++) {
+    console.log(feed[i]);
+    if (feed[i].className === objectType) {
+      feed[i].style.display = 'block';
+    }
+  }
+}
